a0a8c1e6e48717023a729356dfdc241e9a831208
DanielCandiaWR <padilla_da@hotmail.com>
2019-11-11 20:11:40 -0600

    This is a personalized echo, which recive a name and displays the current user and the current working directory

---
+++ personalizedEcho.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Write a Bash script which accepts name as input and displays a greeting: "Welcome (name)"
+read name
+echo "Welcome $name"
+echo "Your actual user is $USER and your current work directory is $PWD"
